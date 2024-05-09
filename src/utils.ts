1607e1997e290dbb55ee07524b7080cd52c95b14
drilon <drilonhametaj25@gmail.com>
2024-05-09 15:38:49 +0200

    add core structure

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ import {
   Cell,
   OpenedContract,
   TonClient,
+  WalletContractV3R2,
   WalletContractV4,
 } from "ton";
 
@@ -29,6 +30,50 @@ export async function openWallet(mnemonic: string[], testnet: boolean) {
     publicKey: keyPair.publicKey,
   });
 
-  const contract = client.open(wallet);
+  let contract = client.open(wallet);
   return { contract, keyPair };
+}
+
+function bufferToChunks(buff: Buffer, chunkSize: number) {
+  const chunks: Buffer[] = [];
+  while (buff.byteLength > 0) {
+    chunks.push(buff.slice(0, chunkSize));
+    buff = buff.slice(chunkSize);
+  }
+  return chunks;
+}
+
+function makeSnakeCell(data: Buffer): Cell {
+  const chunks = bufferToChunks(data, 127);
+
+  if (chunks.length === 0) {
+    return beginCell().endCell();
+  }
+
+  if (chunks.length === 1) {
+    return beginCell().storeBuffer(chunks[0]).endCell();
+  }
+
+  let curCell = beginCell();
+
+  for (let i = chunks.length - 1; i >= 0; i--) {
+    const chunk = chunks[i];
+
+    curCell.storeBuffer(chunk);
+
+    if (i - 1 >= 0) {
+      const nextCell = beginCell();
+      nextCell.storeRef(curCell);
+      curCell = nextCell;
+    }
+  }
+
+  return curCell.endCell();
+}
+
+export function encodeOffChainContent(content: string) {
+  let data = Buffer.from(content);
+  const offChainPrefix = Buffer.from([0x01]);
+  data = Buffer.concat([offChainPrefix, data]);
+  return makeSnakeCell(data);
 }
